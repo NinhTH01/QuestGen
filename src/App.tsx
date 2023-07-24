1962f55ee5f7c098f9ffa9ee4723f4cd7cdef292
Urrrrah <hninh1710@gmail.com>
2023-07-24 23:53:34 +0700

    Add headbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,98 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const mainName = {
+    color: "rgb(20, 60, 244)",
+    fontWeight: "bold",
+  };
+
+  const subColor = {
+    color: "#282a30",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button>This is button</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid flex-row">
+        <a className="navbar-brand" href="https://www.questgen.ai/">
+          <span style={mainName}>QuestGen</span>
         </a>
-      </header>
-    </div>
+        <div className="" id="navbarNavAltMarkup">
+          <nav className="nav">
+            <a
+              className="nav-link active fw-bold"
+              aria-current="page"
+              href="https://www.questgen.ai/"
+              style={subColor}
+            >
+              Home
+            </a>
+            <a
+              className="nav-link active fw-bold"
+              aria-current="page"
+              href="https://www.questgen.ai/"
+              style={subColor}
+            >
+              Pricing
+            </a>
+            <a
+              className="nav-link active fw-bold"
+              aria-current="page"
+              href="https://www.questgen.ai/"
+              style={subColor}
+            >
+              Team
+            </a>
+            <a
+              className="nav-link active fw-bold"
+              aria-current="page"
+              href="https://www.questgen.ai/"
+              style={subColor}
+            >
+              Blog
+            </a>
+            <a
+              className="nav-link active fw-bold"
+              aria-current="page"
+              href="https://www.questgen.ai/"
+              style={subColor}
+            >
+              Contact
+            </a>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="https://www.questgen.ai/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="https://www.questgen.ai/">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="https://www.questgen.ai/">
+                    Another action
+                  </a>
+                </li>
+
+                <li>
+                  <a className="dropdown-item" href="https://www.questgen.ai/">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </nav>
+        </div>
+      </div>
+    </nav>
   );
 }
 
